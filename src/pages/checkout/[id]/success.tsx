7a975909cbb2974cd07c9e560557caa69a578c45
FranciscoGontijo <franciscoacmg@gmail.com>
2023-10-16 08:05:29 -0300

    Success page

---
+++ src/pages/checkout/[id]/success.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+
+type Props = {};
+
+const CheckoutSuccessPage: NextPage = (props: Props) => {
+    return (
+        <div>
+            <h3>Congrats, your purchase was completed</h3>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+};
+
+export default CheckoutSuccessPage;
