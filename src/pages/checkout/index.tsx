3b30413f42b6ff1879688b9f3867cb904ee0a7da
FranciscoGontijo <franciscoacmg@gmail.com>
2023-10-18 17:30:41 -0300

    Checkout page (display products, create order onSubmit and redirect to success)

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,5 +1,8 @@
 import { NextPage } from "next";
-import { FormEvent } from "react";
+import { useRouter } from "next/router";
+import { FormEvent, useContext } from "react";
+import { CartContext } from "@/context/cart.provider";
+import { http } from "@/util/http";
 
 type Props = {
 
@@ -7,13 +10,30 @@ type Props = {
 
 export const CheckoutPage: NextPage = (props: Props) => {
 
-    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const cartContext = useContext(CartContext);
+    
+    const router = useRouter();
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    }
+        const credit_card_number = event.currentTarget.credit_card_number.value;
+        const {data: order} = await http.post('orders', {
+            products: cartContext.products,
+            credit_card_number,
+        });
+        router.push(`/checkout/${order.id}/success`);
+    };
 
     return (
-        <div>
+        <div>           
             <h3>My cart</h3>
+            <ul>
+                {cartContext.products.map((product) => (
+                    <li key={product.id}>
+                        Product {product.name} - {product.price}
+                    </li>
+                ))}
+            </ul>
             <form onSubmit={onSubmit}>
                 <div>
                     <label htmlFor="">Credit Card</label>
